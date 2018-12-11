98cb39b123adca04610b6436a0b4b908133cc22d
Forcada <jorge_forcada@hotmail.com>
2018-12-11 01:03:05 +0100

    Add tests to test the contracts

--- src/test/java/es/upm/grise/profundizacion2018/tema6/CourseDataValuesTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema6/CourseDataValuesTest.java
@@ -4,7 +4,12 @@ import es.upm.grise.profundizacion2018.tema6.values.CourseData;
 
 public enum CourseDataValuesTest implements CourseData {
 	
-	CORRECT_DEGREE_COURSE ("DEGREE", "Correct degree course", 5);
+	CORRECT_DEGREE_COURSE ("DEGREE", "Correct degree course", 3),
+	CORRECT_MASTER_COURSE ("MASTER", "Correct master course", 6),
+	INCORRECT_DEGREE_COURSE_ZERO_CREDITS ("DEGREE", "Incorrect degree course", 0),
+	INCORRECT_DEGREE_COURSE_EXCESIVE_CREDITS ("DEGREE", "Incorrect degree course", 7),
+	INCORRECT_MASTER_COURSE_ZERO_CREDITS ("MASTER", "Incorrect master course", 0),
+	INCORRECT_MASTER_COURSE_EXCESIVE_CREDITS ("MASTER", "Incorrect master course", 7);
 	
 	private String level;
 	private String name;
--- src/test/java/es/upm/grise/profundizacion2018/tema6/SmokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema6/SmokeTest.java
@@ -1,23 +1,151 @@
 package es.upm.grise.profundizacion2018.tema6;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
+import com.google.java.contract.PreconditionError;
+
 import static es.upm.grise.profundizacion2018.tema6.CourseDataValuesTest.*;
+
+import es.upm.grise.profundizacion2018.tema6.course.Course;
 import es.upm.grise.profundizacion2018.tema6.course.DegreeCourse;
 import es.upm.grise.profundizacion2018.tema6.course.MasterCourse;
 
 public class SmokeTest {
 
-	@Test
-	public void degreeCoursesCanBeInstantiatedWithDEGREELevelCoursesOnly() {
+	@Test(expected = Test.None.class)
+	public void degreeCoursesCanBeInstantiatedCorrectly() {
 		
-		// 1 <= registration <= 6 to be valid
 		int NUM_REGISTRATION = 1;
-		
-		// An exception is not raised
-		DegreeCourse degreeCourse = new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
 	}
-
+	
+	@Test(expected = PreconditionError.class)
+	public void degreeCoursesCantBeInstantiatedWithLessThan1NUM_REGISTRATION() {
+		
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void degreeCoursesCantBeInstantiatedWithMoreThan6NUM_REGISTRATION() {
+		
+		int NUM_REGISTRATION = 7;
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = Test.None.class)
+	public void masterCoursesCanBeInstantiatedCorrectly() {
+		
+		int NUM_REGISTRATION = 1;
+		new MasterCourse(CORRECT_MASTER_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void masterCoursesCantBeInstantiatedWithLessThan1NUM_REGISTRATION() {
+		
+		int NUM_REGISTRATION = 0;
+		new MasterCourse(CORRECT_MASTER_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void masterCoursesCantBeInstantiatedWithMoreThan6NUM_REGISTRATION() {
+		
+		int NUM_REGISTRATION = 7;
+		new MasterCourse(CORRECT_MASTER_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void degreeCoursesCantBeInstantiatedWithLessThan1Credits() {
+		
+		int NUM_REGISTRATION = 1;
+		new DegreeCourse(INCORRECT_DEGREE_COURSE_ZERO_CREDITS, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void degreeCoursesCantBeInstantiatedWithMoreThan6Credits() {
+		
+		int NUM_REGISTRATION = 1;
+		new DegreeCourse(INCORRECT_DEGREE_COURSE_EXCESIVE_CREDITS, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void masterCoursesCantBeInstantiatedWithLessThan1Credits() {
+		
+		int NUM_REGISTRATION = 1;
+		new MasterCourse(INCORRECT_DEGREE_COURSE_ZERO_CREDITS, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void masterCoursesCantBeInstantiatedWithMoreThan6Credits() {
+		
+		int NUM_REGISTRATION = 1;
+		new MasterCourse(INCORRECT_DEGREE_COURSE_EXCESIVE_CREDITS, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void degreeCoursesCantBeInstantiatedWithMASTERLevel() {
+		
+		int NUM_REGISTRATION = 1;
+		new DegreeCourse(CORRECT_MASTER_COURSE, NUM_REGISTRATION);
+	}
+	
+	@Test(expected = Test.None.class)
+	public void registrationAddCourseCorrectly() {
+		
+		Registration registration = new Registration();
+		Course course = new DegreeCourse(CORRECT_DEGREE_COURSE, 1);
+		registration.addCourse(course);
+		
+		assertThat(registration.getRegistrationFee()).isEqualTo(course.getFee());
+		assertThat(registration.getTotalCredits()).isEqualTo(course.getCredits());
+		assertThat(registration.getNumberCourses()).isEqualTo(1);
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void registrationErrorWhenAdding11thCourse() {
+		
+		Registration registration = new Registration();
+		
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+	}
+	
+	@Test(expected = PreconditionError.class)
+	public void registrationErrorWhenAddingMoreThan36ECTS() {
+		
+		Registration registration = new Registration();
+		
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 6));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, 1));
+	}
+	
+	//TODO
+	@Test(expected = PreconditionError.class)
+	public void courseCantHaveNegativeFee() {
+		
+		Course course = new DegreeCourse(CORRECT_DEGREE_COURSE, 1);
+	}
+	
+	//TODO
+	@Test(expected = PreconditionError.class)
+	public void registrationCantHaveNegativeFee() {
+		
+		Registration registration = new Registration();
+	}
 }
