5aa7d4bff13b2d8c589f049b03e61af0cbba9b73
Forcada <jorge_forcada@hotmail.com>
2018-12-12 20:10:39 +0100

    Add desired Contracts

--- src/main/java/es/upm/grise/profundizacion2018/tema6/course/Course.java
+++ src/main/java/es/upm/grise/profundizacion2018/tema6/course/Course.java
@@ -9,7 +9,7 @@ public abstract class Course implements Comparable <Course> {
 	protected int numRegistrations;
 	protected CourseData courseData;
 
-	@Requires({"numRegistrations >= 1", "numRegistrations <= 6", "getCredits() >= 1", "getCredits() <= 6", "getFee() >= 0"})
+	@Requires({"numRegistrations >= 1", "numRegistrations <= 6"})
 	public Course(int numRegistrations) {
 		this.numRegistrations = numRegistrations;
 	}
--- src/main/java/es/upm/grise/profundizacion2018/tema6/course/DegreeCourse.java
+++ src/main/java/es/upm/grise/profundizacion2018/tema6/course/DegreeCourse.java
@@ -1,5 +1,6 @@
 package es.upm.grise.profundizacion2018.tema6.course;
 
+import com.google.java.contract.Ensures;
 import com.google.java.contract.Requires;
 
 import es.upm.grise.profundizacion2018.tema6.values.CourseData;
@@ -9,6 +10,7 @@ public class DegreeCourse extends Course {
 	final private static double fee = 27.33;
 
 	@Requires("course.getLevel().equals(\"DEGREE\")")
+	@Ensures({"getCredits() >= 1", "getCredits() <= 6", "getFee() >= 0"})
 	public DegreeCourse(CourseData course, int numRegistrations) {
 		super(numRegistrations);
 		this.courseData = course;
