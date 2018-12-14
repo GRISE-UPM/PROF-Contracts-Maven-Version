a8c329a151c39beeabe7d53ce70580ff20283c9a
AlejandroBudy <atorresato@gmail.com>
2018-12-14 08:29:20 +0100

    Test implemented

--- src/test/java/es/upm/grise/profundizacion2018/tema6/CourseDataValuesTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema6/CourseDataValuesTest.java
@@ -4,8 +4,33 @@ import es.upm.grise.profundizacion2018.tema6.values.CourseData;
 
 public enum CourseDataValuesTest implements CourseData {
 	
-	CORRECT_DEGREE_COURSE ("DEGREE", "Correct degree course", 5);
-	
+	CORRECT_DEGREE_COURSE ("DEGREE", "Correct degree course", 5),
+	INCORRECT_DEGREE_COURSE ("MASTER", "Correct degree course", 5),
+	INCORRECT_MASTER_COURSE ("MASTER", "Correct degree course", 5),
+	INCORRECT_DEGREE_COURSE_MORE_CREDITS ("DEGREE", "Correct degree course",9),
+	INCORRECT_MASTER_COURSE_MORE_CREDITS ("MASTER", "Correct degree course",9),
+	CORRECT_DEGREE_COURSE_1 ("DEGREE", "Correct degree course 1", 5),
+	CORRECT_DEGREE_COURSE_2 ("DEGREE", "Correct degree course 2", 5),
+	CORRECT_DEGREE_COURSE_3 ("DEGREE", "Correct degree course 3", 5),
+	CORRECT_DEGREE_COURSE_4 ("DEGREE", "Correct degree course 4", 5),
+	CORRECT_DEGREE_COURSE_5 ("DEGREE", "Correct degree course 5", 5),
+	CORRECT_DEGREE_COURSE_6 ("DEGREE", "Correct degree course 6", 5),
+	CORRECT_DEGREE_COURSE_7 ("DEGREE", "Correct degree course 7", 5),
+	CORRECT_DEGREE_COURSE_8 ("DEGREE", "Correct degree course 8", 5),
+	CORRECT_MASTER_COURSE_1 ("MASTER", "Correct degree course 1", 1),
+	CORRECT_MASTER_COURSE_2 ("MASTER", "Correct degree course 2", 1),
+	CORRECT_MASTER_COURSE_3 ("MASTER", "Correct degree course 3", 1),
+	CORRECT_MASTER_COURSE_4 ("MASTER", "Correct degree course 4", 1),
+	CORRECT_MASTER_COURSE_5 ("MASTER", "Correct degree course 5", 1),
+	CORRECT_MASTER_COURSE_6 ("MASTER", "Correct degree course 6", 1),
+	CORRECT_MASTER_COURSE_7 ("MASTER", "Correct degree course 7", 1),
+	CORRECT_MASTER_COURSE_8 ("MASTER", "Correct degree course 8", 1),
+	CORRECT_MASTER_COURSE_9 ("MASTER", "Correct degree course 8", 1),
+	CORRECT_MASTER_COURSE_10("MASTER", "Correct degree course 8", 1),
+	CORRECT_MASTER_COURSE_11("MASTER", "Correct degree course 8", 1);
+
+
+
 	private String level;
 	private String name;
 	private int credits;
--- src/test/java/es/upm/grise/profundizacion2018/tema6/SmokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/tema6/SmokeTest.java
@@ -2,6 +2,7 @@ package es.upm.grise.profundizacion2018.tema6;
 
 import static org.junit.Assert.*;
 
+import com.google.java.contract.PreconditionError;
 import org.junit.Test;
 
 import static es.upm.grise.profundizacion2018.tema6.CourseDataValuesTest.*;
@@ -10,14 +11,80 @@ import es.upm.grise.profundizacion2018.tema6.course.MasterCourse;
 
 public class SmokeTest {
 
+
 	@Test
 	public void degreeCoursesCanBeInstantiatedWithDEGREELevelCoursesOnly() {
-		
-		// 1 <= registration <= 6 to be valid
 		int NUM_REGISTRATION = 1;
-		
-		// An exception is not raised
-		DegreeCourse degreeCourse = new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
 	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenNumRegistrationIsLessThanOne() {
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenNumRegistrationIsGreaterThanSix() {
+		int NUM_REGISTRATION = 7;
+		new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenDegreeCourseIsInstantiatedWithMaster() {
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(INCORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenMasterCourseIsInstantiatedWithDegree() {
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(INCORRECT_MASTER_COURSE, NUM_REGISTRATION);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenDegreeCourseHasMoreCredits() {
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(INCORRECT_DEGREE_COURSE_MORE_CREDITS, NUM_REGISTRATION);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailWhenMasterCourseHasMoreCredits() {
+		int NUM_REGISTRATION = 0;
+		new DegreeCourse(INCORRECT_MASTER_COURSE_MORE_CREDITS, NUM_REGISTRATION);
+	}
+	@Test
+	public void shouldRegisterOk(){
+		int NUM_REGISTRATION = 1;
+		DegreeCourse degreeCourse = new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION);
+		Registration registration = new Registration();
+		registration.addCourse(degreeCourse);
+	}
+	@Test(expected = PreconditionError.class)
+	public void shouldFailRegisterWhenCreditsExceedsLimit(){
+		int NUM_REGISTRATION = 1;
+		Registration registration = new Registration();
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_1, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_2, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_3, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_4, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_5, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_6, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_7, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_DEGREE_COURSE_8, NUM_REGISTRATION));
+	}
+
+	@Test(expected = PreconditionError.class)
+	public void shouldFailRegisterWhenNumberOfSignaturesExceedsLimit(){
+		int NUM_REGISTRATION = 1;
+		Registration registration = new Registration();
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_1, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_2, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_3, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_4, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_5, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_6, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_7, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_8, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_9, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_10, NUM_REGISTRATION));
+		registration.addCourse(new DegreeCourse(CORRECT_MASTER_COURSE_11, NUM_REGISTRATION));
+	}
+
 
 }
